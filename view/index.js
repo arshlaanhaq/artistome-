1b7631cbe3c3bba1cee7611382dcd9d8a763a3f7
arshlaanhaq <arshlaanhaq005@.com>
2023-03-04 16:10:16 +0530

    some Changes

--- view/index.js
+++ view/index.js
@@ -1,11 +1,39 @@
-
+// ---------------------------------for-navbar-------------------------------
 document.getElementsByClassName("fa")[4].addEventListener(
     "click", function () {
         document.getElementsByClassName("links")[0].classList.toggle
             ("showlinks")
     })
 
+// ---------------------------------------slider 1----------------------------------------------------------
+ let slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// ****************************************slider2**************************************
 if (document.getElementById("slider--text")) {
 
     let slides = [
@@ -27,4 +55,3 @@ if (document.getElementById("slider--text")) {
     slider(); // Start slider immediately
     setInterval(slider, 4000); // Slide every 4 seconds
 }
-
